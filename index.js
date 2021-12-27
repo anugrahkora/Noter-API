6cde0ff94d6bc818d4f91a685aeabd98a56712f6
Anugrah K <anugrahkorembeth.ak@gmail.com>
2021-12-27 20:41:38 +0530

    added config

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const mongoose= require('mongoose');
 const User=require('./models/userModel');
 const Note=require('./models/noteModel');
 const { response } = require('express');
+const config = require('./config.json');
 // const request = require('request-promise');
 const app=express();
 const PORT= process.env.PORT|| 5000;
-const dbURI='mongodb+srv://NoterClientUser:DontforgetNoterclientuser18@cluster0.njjrw.mongodb.net/userDatabase?retryWrites=true&w=majority';
-mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
+
+mongoose.connect(config.test.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true})
 .then((result)=>app.listen(PORT,()=>console.log('Server running on '+PORT)))
 .catch((err)=>console.log(err));
 
